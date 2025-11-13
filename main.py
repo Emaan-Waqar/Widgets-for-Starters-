9154c4e86e5d41c1cd19e1215e8e22d5005f1312
Emaan Waqar <149870059+Emaan-Waqar@users.noreply.github.com>
2025-11-13 16:53:29 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,8 @@ import tkinter as tk
 window=tk.Tk()
 window.title("GUI WINDOW")
 window.geometry("500x500")
+l=tk.Label(text="Hi",fg="Black",bg="white")
+l.pack()
+b=tk.Button(text="Click me",fg="black", bg="lightgray")
+b.pack()
 window.mainloop()
